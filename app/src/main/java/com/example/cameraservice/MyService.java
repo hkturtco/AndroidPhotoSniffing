649c3b2f8afd77a3dee788fd11576dc4ca6e4bb3
hkturtco <imhktc@gmail.com>
2017-01-08 22:06:53 +0800

    Service class amendment

--- app/src/main/java/com/example/cameraservice/MyService.java
+++ app/src/main/java/com/example/cameraservice/MyService.java
@@ -17,22 +17,6 @@ import java.security.Policy;
 
 public class MyService extends Service implements SurfaceHolder.Callback {
 
-    private Camera camera;
-    private android.hardware.Camera.Parameters para;
-    private Bitmap bmp;
-    FileOutputStream fo;
-    private String FLASH_MODE;
-    private int QUALITY_MODE = 0;
-    private boolean isFrontCamRequest = false;
-    private Camera.Size picSize;
-    SurfaceView sv;
-    private SurfaceHolder sholder;
-    private WindowManager wm;
-    WindowManager.LayoutParams params;
-    public Intent cameraIntent;
-    SharedPreferences pref;
-    SharedPreferences.Editor editor;
-    int width =0, height = 0;
 
 
     public void onCreate(){
@@ -43,28 +27,6 @@ public class MyService extends Service implements SurfaceHolder.Callback {
     @Override
     public IBinder onBind(Intent intent) {
         return null;
-    }
-
-    private Camera openFrontFacingCamera(){
-        if (camera !=null){
-            camera.stopPreview();
-            camera.release();
-        }
-        int cameraCount = 0;
-        Camera cam = null;
-        Camera.CameraInfo caminfo = new Camera.CameraInfo();
-        cameraCount = Camera.getNumberOfCameras();
-        for (int camin = 0; camin < cameraCount; camin++){
-            Camera.getCameraInfo(camin, caminfo);
-            if(caminfo.facing == Camera.CameraInfo.CAMERA_FACING_FRONT){
-                try{
-                    cam = Camera.open(camin);
-                } catch (RuntimeException e){
-                    Log.e("Camera","Failed to open Camera");
-                }
-            }
-        }
-        return cam;
     }
 
     @Override
